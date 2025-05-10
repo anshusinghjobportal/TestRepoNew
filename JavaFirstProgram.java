fb6adca9c9595d2b76539a637da878d61d9b12da
anshuman <anshusinghjobportal@gmail.com>
2025-05-10 14:18:59 +0530

    FirstCommit

---
+++ JavaFirstProgram.java
@@ -0,0 +1,13 @@
+class JavaFirstProgram {
+
+    public static void main(String args[]) {
+
+        System.out.println("This is first java Program"); // <-- semicolon added here
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("This is current i value " + i);
+        }
+
+    }
+
+}
